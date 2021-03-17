446ed8f421ec351dd2610a124b63977cc6601090
Ben Stein <whereyouben@gmail.com>
2021-03-17 02:24:19 -0400

    added getInitialProps to _app that checks for more getInitialProps in the subpages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,31 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.scss';
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/index.scss';
+
+import Navbar from '@/components/shared/Navbar';
+import Hero from '@/components/shared/Hero';
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div className="portfolio-app">
+      <Navbar />
+      {pageProps.appData}
+      {Component.name === 'Home' && <Hero />}
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }  
+
+MyApp.getInitialProps = async (context) => {
+  const initialProps = App.getInitialProps && await App.getInitialProps(context);
+
+  console.log(initialProps)
+
+  return {pageProps: {appData: 'Hello _App Component', ...initialProps.pageProps}};
+}
 
 export default MyApp;
