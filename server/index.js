c1cb77bac892308362adb3941d0276687d26692b
Ben Stein <whereyouben@gmail.com>
2021-03-23 13:37:42 -0400

    started developing data/schema/root

--- server/index.js
+++ server/index.js
@@ -8,13 +8,55 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+
+const data = {
+  portfolios: [
+    {
+      _id: '74ghik21',
+      title: 'Job in USA',
+      content: 'It was very nice experience',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false
+    },
+    {
+      _id: 'aln74h01m',
+      title: 'Job in Barcelona',
+      content: 'It was very sunny experience',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: true
+    },
+    {
+      _id: '3m1lm90',
+      title: 'Job in Germany',
+      content: 'It was very good!',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true
+    },
+  ]
+}
+
+
 app.prepare().then(() => {
   const server = express()
 
   //construct a schema using graphql schema language
   const schema = buildSchema(`
+
+    type Portfolio {
+      _id: ID!
+      title: String
+      content: String
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentlyEmployed: Boolean
+    }
+
     type Query {
       hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
     }
   `)
 
@@ -22,6 +64,12 @@ app.prepare().then(() => {
   const root = {
     hello: () => {
       return "Hello World!"
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
