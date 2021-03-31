393502a602516e569a57f186598741e393805120
Ben Stein <whereyouben@gmail.com>
2021-03-31 18:48:47 -0400

    set up portfolio detail page

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ app.prepare().then(() => {
 
     type Query {
       hello: String
-      portfolio: Portfolio
+      portfolio(id: ID): Portfolio
       portfolios: [Portfolio]
     }
   `)
@@ -78,8 +78,9 @@ app.prepare().then(() => {
     hello: () => {
       return "Hello World!"
     },
-    portfolio: () => {
-      return data.portfolios[0]
+    portfolio: ({id}) => {
+      const portfolio = data.portfolios.find(p => p._id === id)
+      return portfolio
     },
     portfolios: () => {
       return data.portfolios
