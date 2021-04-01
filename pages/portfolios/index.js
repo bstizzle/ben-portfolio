a36f269f2be450e5bf41040eeb087dd13159ab66
Ben Stein <whereyouben@gmail.com>
2021-04-01 16:40:01 -0400

    added state management for create and update portfolios

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import axios from 'axios';
 import PortfolioCard from '@/components/portfolios/PortfolioCard';
 import Link from 'next/link';
@@ -7,14 +7,14 @@ const fetchPortfolios = () => {
   const query = `
     query Portfolios {
       portfolios {
-        _id,
-        title,
-        company,
-        companyWebsite,
-        location,
-        jobTitle,
-        description,
-        startDate,
+        _id
+        title
+        company
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
         endDate
       }
     }`;
@@ -23,7 +23,73 @@ const fetchPortfolios = () => {
     .then(data => data.portfolios)
 }
 
-const Portfolios = ({ portfolios }) => {
+const graphCreatePortfolio = () => {
+  const query = `
+    mutation CreatedPortfolio {
+      createPortfolio(input: {
+        title: "new job"
+        company: "new company"
+        companyWebsite: "new website"
+        location: "new location"
+        jobTitle: "new job title"
+        description: "new desc"
+        startDate: "12/12/2012"
+        endDate: "11/14/2013"
+      }) {
+        _id
+        title
+        company
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+  return axios.post('http://localhost:3000/graphql', { query })
+    .then(({data: graph}) => graph.data)
+    .then(data => data.createPortfolio)
+}
+
+const graphUpdatePortfolio = (id) => {
+  const query = `
+    mutation UpdatePortfolio {
+      updatePortfolio(id: "${id}", input: {
+        title: "updated job"
+      }) {
+        _id
+        title
+        company
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+  return axios.post('http://localhost:3000/graphql', { query })
+    .then(({data: graph}) => graph.data)
+    .then(data => data.updatePortfolio)
+}
+
+const Portfolios = ({ data }) => {
+  const [portfolios, setPortfolios] = useState(data.portfolios);
+
+  const createPortfolio = async () => {
+    const newPortfolio = await graphCreatePortfolio();
+    const newPortfolios = [...portfolios, newPortfolio];
+    setPortfolios(newPortfolios);
+  }
+
+  const updatePortfolio = async (id) => {
+    const updatedPortfolio = await graphUpdatePortfolio(id);
+    const index = portfolios.findIndex(p => p._id === id);
+    const updatedPortfolios = portfolios.slice();
+    updatedPortfolios[index] = updatedPortfolio;
+    setPortfolios(updatedPortfolios);
+  }
 
   return(
     <>
@@ -33,6 +99,9 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button 
+          onClick={createPortfolio}
+          className="btn btn-primary">Create Portfolio</button>
       </section>
       <section className="pb-5">
         <div className="row">
@@ -45,6 +114,9 @@ const Portfolios = ({ portfolios }) => {
                   <PortfolioCard portfolio={portfolio} />
                 </a>
               </Link>
+              <button
+                className="btn btn-secondary" 
+                onClick={() => updatePortfolio(portfolio._id)}>Update</button>
             </div>
           )}
         </div>
@@ -55,7 +127,7 @@ const Portfolios = ({ portfolios }) => {
 
 Portfolios.getInitialProps = async () => {
   const portfolios = await fetchPortfolios();
-  return { portfolios };
+  return { data: {portfolios} };
 }
 
 export default Portfolios;
