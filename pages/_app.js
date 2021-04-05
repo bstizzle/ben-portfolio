081cdd1814aaf25087060b9ca811111040b89c5a
Ben Stein <whereyouben@gmail.com>
2021-04-05 15:09:03 -0400

    integerated apollo react client to portfolio [id] page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 
-import App from 'next/app';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import 'isomorphic-unfetch';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/index.scss';
@@ -8,19 +10,25 @@ import Navbar from '@/components/shared/Navbar';
 import Hero from '@/components/shared/Hero';
 import Footer from '@/components/shared/Footer';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+})
+
 const MyApp = ({ Component, pageProps }) => {
 
   const isHomePage = () => Component.name === 'Home';
 
   return (
-    <div className="portfolio-app">
-      <Navbar />
-      {isHomePage() && <Hero />}
-      <div className="container">
-        <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <div className="portfolio-app">
+        <Navbar />
+        {isHomePage() && <Hero />}
+        <div className="container">
+          <Component {...pageProps} />
+        </div>
+        {isHomePage() && <Footer />}
       </div>
-      {isHomePage() && <Footer />}
-    </div>
+    </ApolloProvider>
   )
 }  
 
