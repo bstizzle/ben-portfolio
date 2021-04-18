cc0687b30818803eae8ae9c846a14bebe1790660
Ben Stein <whereyouben@gmail.com>
2021-04-18 13:44:54 -0400

    set up functions and script to populate mongoDB

--- server/fakeDb/populate.js
+++ server/fakeDb/populate.js
@@ -6,7 +6,9 @@ const fakeDb = require('./FakeDb');
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, () => {
+}, async () => {
   console.log('Populating DB......');
-  console.log('connected to DB');
+  await fakeDb.populate();
+  await mongoose.connection.close();
+  console.log('DB populated.');
 })
