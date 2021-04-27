b13037799cb3eb8bdd009bfa634f32e62cdc6c2e
Ben Stein <whereyouben@gmail.com>
2021-04-27 14:01:02 -0400

    set up mongoose db model for user

--- server/fakeDb/populate.js
+++ server/fakeDb/populate.js
@@ -4,7 +4,9 @@ const fakeDb = require('./FakeDb');
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 }, async () => {
   console.log('Populating DB......');
   await fakeDb.populate();
